6d42811a3f57ed05b53c8a51b2e8413aff3a2c85
addtheletters <bzhang.2003@gmail.com>
2015-03-01 10:08:21 -0500

    smaller drag?

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ var Ball = function(x, y, radius, color, id, parent){
 	this.id = id;
 	this.color = color;
 
-	this.drag = 0.1;
+	this.drag = 0.01;
 
 	this.parent = parent;
 }
 	Ball.prototype.update = function(delta){
 		this.pos = vec.sum(this.pos, vec.scale(this.vel, delta));
 		var mag = vec.magnitude(this.vel);
+		//console.log(mag);
 		if(mag > 0){
 			this.vel = vec.scale(this.vel, 1-this.drag); //drag
 		}
