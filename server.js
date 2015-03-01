7960840a5872d6144c5147b3f14aee21a804ceb1
addtheletters <bzhang.2003@gmail.com>
2015-03-01 10:01:53 -0500

    scaling slower

--- server.js
+++ server.js
@@ -295,7 +295,7 @@ game = new Game(new fabric.Canvas("c"), vec.createVec(0,0), vec.createVec(1000, 
 //game.addBall(420, 120, 20, 'green', 1);
 
 gameinterval = game.startGame();
-inputScale = 5;
+inputScale = 2;
 
 pss = new PSServer("ws://pilotdcrelay.herokuapp.com");
 socketToBallID = {}; //{PSC:ball}
@@ -304,7 +304,7 @@ ballIDToSocket = {}; //{ball:PSC}
 colors = ['blue', 'green', 'red', 'yellow', 'magenta', 'cyan'];
 colorUses = [0, 0, 0, 0, 0, 0];
 
-splitBufferSize = 2;
+splitBufferSize = 3;
 
 /*function uniqueID(){
   	return '_' + Math.random().toString(36).substr(2, 9);
